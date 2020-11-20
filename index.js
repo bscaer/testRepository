017e520a35805293a5de0c5738985f4889c3c27c
Beth Scaer <bscaer@gmail.com>
2020-11-20 18:25:18 -0500

    Update

---
+++ index.js
@@ -0,0 +1,49 @@
+let itemPrice1 = 10;
+let amountInWallet1 = 50;
+let numberOfFriends1 = 5;
+let ageInYears1 = 10;
+let firstName1 = "John";
+let lastName1 = "Smith";
+let middleInitial1 = "X";
+
+let newAmountInWallet1 = amountInWallet1 - itemPrice1;
+let numFriendsEachYear1 = numberOfFriends1 / ageInYears1;
+let fullName1 = firstName1 + " " + middleInitial1 + ". " + lastName1;
+
+console.log("First run")
+console.log("Item price is " + itemPrice1);
+console.log("Amount in wallet is " + amountInWallet1);
+console.log("The new amount in the wallet is " + newAmountInWallet1 + ".");
+console.log("The number of friends is " + numberOfFriends1);
+console.log("The age in years is " + ageInYears1);
+console.log("The number of friends made per year is " + numFriendsEachYear1 + ".");
+console.log("The first name is " + firstName1);
+console.log("The last name is " + lastName1);
+console.log("The middle initial is " + middleInitial1);
+console.log("The full name is " + fullName1);
+
+let itemPrice2 = 25;
+let amountInWallet2 = 999;
+let numberOfFriends2 = 1000;
+let ageInYears2 = 75;
+let firstName2 = "Betty";
+let lastName2 = "White";
+let middleInitial2 = "Y";
+
+let newAmountInWallet2 = amountInWallet2 - itemPrice2;
+let numFriendsEachYear2 = numberOfFriends2 / ageInYears2;
+let fullName2 = firstName2 + " " + middleInitial2 + ". " + lastName2;
+
+
+console.log("");
+console.log("Second run")
+console.log("Item price is " + itemPrice2);
+console.log("Amount in wallet is " + amountInWallet2);
+console.log("The new amount in the wallet is " + newAmountInWallet2 + ".");
+console.log("The number of friends is " + numberOfFriends2);
+console.log("The age in years is " + ageInYears2);
+console.log("The number of friends made per year is " + numFriendsEachYear2 + ".");
+console.log("The first name is " + firstName2);
+console.log("The last name is " + lastName2);
+console.log("The middle initial is " + middleInitial2);
+console.log("The full name is " + fullName2);
